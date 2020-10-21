42a49c31cf2e139d1934052e30dc56ffaec3b388
jleilani <jxleilani@gmail.com>
2020-10-21 00:08:43 -0400

    successfully updating employee first and last

--- tracker.js
+++ tracker.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { empty } = require("rxjs");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -27,11 +28,11 @@ function start() {
         .then(function(answer) {
           switch (answer.start) {
             case 'View all employees':
-              return console.log("You want to view employees");
+              return viewEmployees();   
             case 'Add Employee':
-              return console.log("Hi");
+              return addEmployee();
             case 'Update Employee':
-              return console.log("Hi");
+              return updateEmployee();
             case 'Remove Employee':
               return console.log("Hi");
             case 'View All Roles':
@@ -47,4 +48,107 @@ function start() {
             case 'Update Department':
           }
         });
+}
+
+function viewEmployees(){
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+  });
+}
+
+function addEmployee(){
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'firstname',
+        message: "Enter the employee's first name:"
+      },
+      {
+        type: 'input',
+        name: 'lastname',
+        message: "Enter the employee's last name:"
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: "Select the employee's role:"
+      }
+    ])
+    .then(function(res){
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: res.firstname,
+          last_name: res.lastname
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Employee successfully updated.");
+          start();
+        }
+      );
+    });
+}
+
+function updateEmployee(){
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+        type: 'list',
+        name: 'selectEmp',
+        message: 'Who would you like to update?',
+        choices: ()=> {
+          let choices = [];
+          for(let i=0;i<res.length;i++){
+            choices.push(res[i].first_name);
+          }
+          return choices;
+        }
+      },
+      {
+        type: 'list',
+        name: 'updateEmp',
+        message: 'What would you like to update?',
+        choices: ['first name', 'last name', 'role', 'manager']
+      }
+    ])
+      .then(function (res){
+        console.log(res.selectEmp);
+        if(res.updateEmp === 'first name'){
+          updateFirstName();
+        }
+      });
+  });
+}
+
+function updateFirstName(){
+  inquirer.prompt({
+    type: 'input',
+    name: 'updateEmpFirst',
+    message: "Enter employee's first name"
+  })
+  .then(function(emp){
+    connection.query(
+      "UPDATE employee SET ? WHERE ?",
+      [
+        {
+          first_name: emp.updateEmpFirst
+        },
+        {
+          id: res.selectEmp.id
+        }
+      ],
+      function(error) {
+        if (error) throw err;
+        console.log(`First name changed to ${emp.updateEmpFirst}`);
+        start();
+      }
+    );
+  });
 }
